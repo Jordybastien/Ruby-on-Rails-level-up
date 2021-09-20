3dcbcde78068002a4296e112bbf4855101e4ceb2
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2021-09-20 16:55:46 +0200

    adding tests

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid wiith valid attributes" do
+    entry = Entry.new(meal_type:'Breakfast', carbohydrates:35, proteins:2, fats:10, calories: 400)
+    assert entry.save
+  end
+  test "should not save entry without calories" do
+    entry = Entry.new(meal_type:'Breakfast', carbohydrates:35, proteins:2, fats:10,)
+    assert_not entry.save
+  end
 end
