3bf4619ec398a5d3ba355ba1de2e1fe18fa921cb
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2021-09-20 15:42:52 +0200

    foodlog

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'archives/index'
+  resources :entries
+  root to: "entries#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
